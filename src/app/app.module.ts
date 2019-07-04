345323ecf262393f2dcc2f77946f94fb9585138d
chouaibemir <emir.chouaib@esprit.tn>
2019-07-04 16:59:21 +0100

    First commit :pray:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { Contacts } from '@ionic-native/contacts';
+import { WebView } from '@ionic-native/ionic-webview/ngx';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -15,6 +17,8 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
+    Contacts,
+    WebView,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Contact, ContactField, ContactName, Contacts } from '@ionic-native/contacts';
+import { WebView } from '@ionic-native/ionic-webview/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,30 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private win: WebView,private contacts: Contacts, private sanitizer: DomSanitizer) {}
+  contactList = [];
 
+  getContacts(): void {
+    this.contacts.find(
+      ["displayName", "phoneNumbers","photos"],
+      {multiple: true, hasPhoneNumber: true}
+      ).then((contacts) => {
+        for (var i=0 ; i < contacts.length; i++){
+          if(contacts[i].displayName !== null) {
+            var contact = {};
+            contact["name"]   = contacts[i].displayName;
+            contact["number"] = contacts[i].phoneNumbers[0].value;
+            if(contacts[i].photos != null) {
+              console.log(contacts[i].photos);
+              
+              contact["image"] = this.win.convertFileSrc(contacts[i].photos[0].value);
+              console.log(contact);
+            } else {
+              contact["image"] = "assets/dummy-profile-pic.png";
+            }
+            this.contactList.push(contact);
+          }
+        }
+    });
+  }
 }
